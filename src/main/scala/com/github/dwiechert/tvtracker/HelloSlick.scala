aa6e0a4bdfef2acc4aad7a384be7f922e47b68cf
dwiechert <dan.wiechert@gmail.com>
2015-04-24 22:16:38 -0500

    Added comments.

--- src/main/scala/com/github/dwiechert/tvtracker/HelloSlick.scala
+++ src/main/scala/com/github/dwiechert/tvtracker/HelloSlick.scala
@@ -2,21 +2,25 @@ package com.github.dwiechert.tvtracker
 
 import scala.slick.driver.PostgresDriver.simple._
 import scala.slick.jdbc.meta.MTable
+import scala.slick.lifted.AbstractTable
 
+/**
+ * Main enterance to using Slick.
+ */
 object HelloSlick extends App {
+  // Define our table query items
   val users = TableQuery[Users]
   val shows = TableQuery[Shows]
   val seasons = TableQuery[Seasons]
 
-  // Create a connection (called a "session") to a PostgreSQL database
+  // Connect to local PostgreSQL database
   val db = Database.forURL("jdbc:postgresql://localhost/tv-show-tracker", driver = "org.postgresql.Driver", user = "tv-show-tracker-role", password = "tracker")
   db.withSession { implicit session =>
 
-    // Create the schema by combining the DDLs for the Suppliers and Coffees
-    // tables using the query interfaces
+    // Create the tables if they don't already exist
     createIfNotExists(users, shows, seasons)
 
-    /* Create / Insert */
+    // Insert some users
     users.insert(
       User("John Doe")
     )
@@ -24,40 +28,53 @@ object HelloSlick extends App {
       User("Fred Smith")
     )
 
+    // Query and delete some users
     val query = for {
       u <- users
       if (u.id < 50)
     } yield (u)
     query.delete
 
+    // Insert some shows
     insertIfNotExists(Show("Oz"))
     insertIfNotExists(Show("Breaking Bad"))
 
+    // Insert some seasons
     insertIfNotExists(Season(1, "Oz"))
     insertIfNotExists(Season(2, "Oz"))
     insertIfNotExists(Season(1, "Breaking Bad"))
 
+    // Pair shows with the seasons
     val joinQuery = for {
       show <- shows
       season <- seasons
       if (show.name === season.showName)
     } yield (show, season)
 
+    // Print out all shows and their seasons
     for {
       (show, season) <- joinQuery
     } println(s"Show: $show\tSeason: $season")
   }
 
+  /**
+   * Create a table if it doesn't already exist.
+   */
   def createIfNotExists(tables: TableQuery[_ <: Table[_]]*)(implicit session: Session) {
     tables foreach { table => if (MTable.getTables(table.baseTableRow.tableName).list.isEmpty) table.ddl.create }
   }
 
+  /**
+   * Insert a show if it doesn't already exist.
+   */
   def insertIfNotExists(show: Show)(implicit session: Session) {
     if (!(shows.map(s => s.name).list.contains(show.name))) shows.insert(show)
   }
 
+  /**
+   * Insert a season if it doesn't already exist.
+   */
   def insertIfNotExists(season: Season)(implicit session: Session) {
     if (!(seasons.map { s => (s.showName, s.number) }.list.contains((season.showName, season.number)))) seasons.insert(season)
-    //    if (!(seasons.filter(s => s.showName === season.showName).map(s => s.number).list.contains(season.number))) seasons.insert(season)
   }
 }
--- src/main/scala/com/github/dwiechert/tvtracker/Tables.scala
+++ src/main/scala/com/github/dwiechert/tvtracker/Tables.scala
@@ -3,25 +3,46 @@ package com.github.dwiechert.tvtracker
 import scala.slick.driver.PostgresDriver.simple._
 import scala.slick.jdbc.JdbcBackend
 
+/**
+ * Case class to define a User.
+ */
 case class User(name: String, id: Option[Int] = None)
+
+/**
+ * Class to define the Users table.
+ */
 class Users(tag: Tag) extends Table[User](tag, "USERS") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def name = column[String]("NAME", O.NotNull)
   def * = (name, id.?) <> (User.tupled, User.unapply)
 }
 
+/**
+ * Case class to define a Show.
+ */
 case class Show(name: String)
+
+/**
+ * Class to define the Shows table.
+ */
 class Shows(tag: Tag) extends Table[Show](tag, "SHOWS") {
   def name = column[String]("NAME", O.PrimaryKey, O.NotNull)
   def * = (name) <> (Show, Show.unapply)
 }
 
+/**
+ * Case class to define a Season.
+ */
 case class Season(number: Int, showName: String, id: Option[Int] = None)
+
+/**
+ * Class to define the Seasons table.
+ */
 class Seasons(tag: Tag) extends Table[Season](tag, "SEASONS") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def number = column[Int]("NUMBER")
   def showName = column[String]("SHOW_NAME")
   def * = (number, showName, id.?) <> (Season.tupled, Season.unapply)
-  
+
   def show = foreignKey("SHOW_FK", showName, TableQuery[Shows])(_.name)
 }
