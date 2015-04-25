dff6d4cbc97456bc7ec630d237069292017d341f
dwiechert <dan.wiechert@gmail.com>
2015-04-24 21:59:11 -0500

    Got Shows and Seasons working and joining together.

--- src/main/scala/com/github/dwiechert/tvtracker/HelloSlick.scala
+++ src/main/scala/com/github/dwiechert/tvtracker/HelloSlick.scala
@@ -4,8 +4,9 @@ import scala.slick.driver.PostgresDriver.simple._
 import scala.slick.jdbc.meta.MTable
 
 object HelloSlick extends App {
-  // the base query for the Users table
   val users = TableQuery[Users]
+  val shows = TableQuery[Shows]
+  val seasons = TableQuery[Seasons]
 
   // Create a connection (called a "session") to a PostgreSQL database
   val db = Database.forURL("jdbc:postgresql://localhost/tv-show-tracker", driver = "org.postgresql.Driver", user = "tv-show-tracker-role", password = "tracker")
@@ -13,7 +14,7 @@ object HelloSlick extends App {
 
     // Create the schema by combining the DDLs for the Suppliers and Coffees
     // tables using the query interfaces
-    createIfNotExists(users)
+    createIfNotExists(users, shows, seasons)
 
     /* Create / Insert */
     users.insert(
@@ -28,9 +29,35 @@ object HelloSlick extends App {
       if (u.id < 50)
     } yield (u)
     query.delete
+
+    insertIfNotExists(Show("Oz"))
+    insertIfNotExists(Show("Breaking Bad"))
+
+    insertIfNotExists(Season(1, "Oz"))
+    insertIfNotExists(Season(2, "Oz"))
+    insertIfNotExists(Season(1, "Breaking Bad"))
+
+    val joinQuery = for {
+      show <- shows
+      season <- seasons
+      if (show.name === season.showName)
+    } yield (show, season)
+
+    for {
+      (show, season) <- joinQuery
+    } println(s"Show: $show\tSeason: $season")
   }
 
   def createIfNotExists(tables: TableQuery[_ <: Table[_]]*)(implicit session: Session) {
     tables foreach { table => if (MTable.getTables(table.baseTableRow.tableName).list.isEmpty) table.ddl.create }
+  }
+
+  def insertIfNotExists(show: Show)(implicit session: Session) {
+    if (!(shows.map(s => s.name).list.contains(show.name))) shows.insert(show)
+  }
+
+  def insertIfNotExists(season: Season)(implicit session: Session) {
+    if (!(seasons.map { s => (s.showName, s.number) }.list.contains((season.showName, season.number)))) seasons.insert(season)
+    //    if (!(seasons.filter(s => s.showName === season.showName).map(s => s.number).list.contains(season.number))) seasons.insert(season)
   }
 }
--- src/main/scala/com/github/dwiechert/tvtracker/Tables.scala
+++ src/main/scala/com/github/dwiechert/tvtracker/Tables.scala
@@ -1,15 +1,27 @@
 package com.github.dwiechert.tvtracker
 
 import scala.slick.driver.PostgresDriver.simple._
-import scala.slick.lifted.ForeignKeyQuery
+import scala.slick.jdbc.JdbcBackend
 
 case class User(name: String, id: Option[Int] = None)
 class Users(tag: Tag) extends Table[User](tag, "USERS") {
-  // Auto Increment the id primary key column
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-  // The name can't be null
   def name = column[String]("NAME", O.NotNull)
-  // the * projection (e.g. select * ...) auto-transforms the tupled
-  // column values to / from a User
   def * = (name, id.?) <> (User.tupled, User.unapply)
+}
+
+case class Show(name: String)
+class Shows(tag: Tag) extends Table[Show](tag, "SHOWS") {
+  def name = column[String]("NAME", O.PrimaryKey, O.NotNull)
+  def * = (name) <> (Show, Show.unapply)
+}
+
+case class Season(number: Int, showName: String, id: Option[Int] = None)
+class Seasons(tag: Tag) extends Table[Season](tag, "SEASONS") {
+  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+  def number = column[Int]("NUMBER")
+  def showName = column[String]("SHOW_NAME")
+  def * = (number, showName, id.?) <> (Season.tupled, Season.unapply)
+  
+  def show = foreignKey("SHOW_FK", showName, TableQuery[Shows])(_.name)
 }
